48cffece8a2f53c5d418f36a1fab59c39daa5f48
Lucas Ortis <lucasortis01@gmail.com>
2021-08-26 13:30:21 -0300

    Adicionando data local

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -3,6 +3,8 @@
 namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine(DateTime.Now);
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
             Console.ReadLine();
