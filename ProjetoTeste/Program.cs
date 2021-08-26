b516df02bc1dd13fd8b8a5c5bca1e6abb307b9c8
Lucas Ortis <lucasortis01@gmail.com>
2021-08-26 14:00:38 -0300

    Melhorando mensagem de bom dia e adicionando linhas

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -4,9 +4,12 @@ namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
 
+
+            Console.WriteLine("========================");
             Console.WriteLine(DateTime.Now);
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Bom dia, como vai?");
+            Console.WriteLine("========================");
             Console.ReadLine();
         }
     }
